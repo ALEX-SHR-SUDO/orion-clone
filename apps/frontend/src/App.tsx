a2418dbe242d6ee4dbdf1c00dddd4bdfda9f613b
ALEX-SHR-SUDO <alexshargo@icloud.com>
2025-09-12 16:04:11 +0000

    next

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateTokenPage from './pages/CreateTokenPage'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <CreateTokenPage />
+    </div>
+  )
+}
+
+export default App
